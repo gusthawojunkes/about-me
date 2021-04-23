218576f631a3b2b4af78b89b91e4618c7fb66050
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2021-04-22 22:02:09 -0300

    WIP: Age calc and headings

--- main.js
+++ main.js
@@ -1,36 +1,52 @@
-const CONTENT_POS = [
-    ABOUT_POS = 0,
-    SCHOOLING_POS = 1
-]
+const DEFAULT_CONTENT = {
+    full_name: "Gusthawo Junkes",
+    f_name: "Gusthawo",
+    l_name: "Junkes",
+    getAge: () => {
+        born_date = new Date("2002/10/17")
+        return Math.trunc((new Date() - born_date) / (31557600000))
+    }
+}
 
 getContentByLang = (lang, type) => {
     for (item in lang[type]) document.getElementById(`info-${type}`).innerHTML += `<li>- ${lang[type][item]}</li>`
 }
 
 switchLang = (lang) => {
+    let language = lang.toString() == 'EN_US' ? EN_US : PT_BR
+    let headLang = lang.toString() == 'EN_US' ? HEADING_EN_US : HEADING_PT_BR
+
+    document.documentElement.lang = lang.toString() == 'EN_US' ? 'en' : 'pt-br'
+
     for (index in CONTENT_POS) {
         document.getElementById(`info-${index}`).innerHTML = ''
-        getContentByLang(lang == 'EN_US' ? EN_US : PT_BR, index)
+        getContentByLang(language, index)
+    }
+
+    for (index in HEADINGS) {
+        let element = document.getElementById(HEADINGS[index])
+        element.innerHTML = ''
+        element.innerHTML += headLang[index]
     }
 }
 
-const DEFAULT_CONTENT = {
-    full_name: "Gusthawo Junkes",
-    f_name: "Gusthawo",
-    l_name: "Junkes",
-    getAge: () => {
-        let today = new Date()
-        format_date = (date) => {
-            return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
-        }
-        born_date = new Date("2002/10/17")
-        return format_date(today)
-    }
+format_date = (date) => {
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`
 }
+
+const HEADINGS = ['heading-0', 'heading-connect', 'heading-1']
+
+const CONTENT_POS = [
+    ABOUT_POS = 0,
+    SCHOOLING_POS = 1
+]
+
+const HEADING_PT_BR = ['Sobre mim', 'Conecte-se comigo!', 'Escolaridade']
+const HEADING_EN_US = ['About me', 'Connect with me!', 'Schooling']
 
 const EN_US = [
-    about = {
-        born_text: 'I was born in 2002',
+    about = { 
+        born_text: 'I was born in 2002, I am currently ' + DEFAULT_CONTENT.getAge() + ' years old',
         info1: 'My first contact with programming was in 2018',
         info2: 'The first programming language I learned was Python',
         info3: 'I always prefer the dark theme'
@@ -42,7 +58,7 @@ const EN_US = [
 
 const PT_BR = [
     about = {
-        born_text: 'Eu nasci em 2002',
+        born_text: 'Eu nasci em 2002, atualmente eu tenho ' + DEFAULT_CONTENT.getAge() + ' anos',
         info1: 'Meu primeiro contato com programação foi em 2018',
         info2: 'A primeira linguagem de programação que eu aprendi foi Python',
         info3: 'Eu sempre prefiro o tema escuro'
@@ -52,4 +68,5 @@ const PT_BR = [
     }
 ]
 
+document.getElementById('nameAndAge').innerHTML += DEFAULT_CONTENT.full_name
 for (index in CONTENT_POS) getContentByLang(PT_BR, index)
